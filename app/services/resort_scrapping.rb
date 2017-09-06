e06d46d15ae9e251f364014d4b9876f8cf5f822b
isarceaux <isarceaux>
2017-09-06 16:43:57 +0200

    change page started, problem found on location identification, started to investigate a way to find it with regex in scripts

--- app/services/resort_scrapping.rb
+++ app/services/resort_scrapping.rb
@@ -1,13 +1,15 @@
 class ResortScrapping
 
-  attr_accessor :url, :url_dates, :page
+  attr_accessor :url, :url_dates, :url_pages, :page, :start_date
 
   def initialize
     p "hello world !"
     # bob = Classified.new
     # bob.save
+    @start_date = Date.new
     # getDataFromPage
-    changeDate
+    # changeDate
+    changePage
   end
 
   def changeResort
@@ -18,11 +20,12 @@ class ResortScrapping
     season_end = Date.new(2018,5,5)
     number_of_weeks = (season_end-season_start)/7
     number_of_weeks = number_of_weeks.to_i
-    start_date = Date.new
+    
+    binding.pry
 
     number_of_weeks.times do |i|
       #change url
-      start_date = season_start + (i-1)*7
+      @start_date = season_start + (i-1)*7
       @url_dates = "arrival:" + start_date.strftime + "/departure:" + (start_date+7).strftime + "/"
       changePage
     end
@@ -31,41 +34,51 @@ class ResortScrapping
   
   def changePage
     puts @url_dates
+    @url_pages=""
+    # getDataFromPage
+    differentPages = true
+    counter = 2
+    # while differentPages 
+
+    # end 
+    binding.pry
+    local_old_url = "https://www.abritel.fr/annonces/location-vacances/france_provence-alpes-cote%20d%27azur_4_le%20sauze_dt0.php/"
+    new_page = Nokogiri::HTML(open(local_old_url))
+    puts new_page.base_uri.to_s
+
   end
 
   def getDataFromPage
+    
     @url = "https://www.abritel.fr/results/rhone-alpes/champagny-en-vanoise/region:66612899/arrival:2017-12-16/departure:2017-12-23/minSleeps/1"
     @page = Nokogiri::HTML(open(@url))
+
     xpath_name = '//h3[@class="hit-headline"]'
     xpath_url = '//h3[@class="hit-headline"]/a'
-    # xpath_prices = '//div[@class="rate"]'
     xpath_prices = "//div[@class='rate']/a"
+    xpath_guests = '//li[@class="accommodation accommodation--simple bbs-sleeps"]/div[@class="bd-bth-slps-count"]'
+
+    #For location use of regex :
+    # reg = /"geography":{"name":(.*)"location"/
+    # reg = /^geography(.*)location$/
+    # reg = /\A"geography".*France","location"\Z/
+    # reg = /\Ageography.*France\Z/
+    # reg = /geography.*France/
 
     names = page.xpath(xpath_name)
     urls = page.xpath(xpath_url)
     prices = page.xpath(xpath_prices)
-    # prices does return an empty array for now, several unsucessful attempts, next step try using a regex and collect of bigger split of raw html
+    guests = page.xpath(xpath_guests)
 
-    names.each do |n|
-      puts n.text.slice(14..-10)
-      # puts element['href']
+    names.length.times do |i|
+      Classified.create(startDate:@start_date,
+        endDate:@start_date+7,
+        title:names[i-1].text.slice(14..-10),
+        price:prices[i-1].text.slice(0..-3).to_f,
+        link:urls[i-1]["href"],
+        numberOfGuests:guests[i-1].text.to_i)
     end
-
-    urls.each do |u|
-      puts u["href"]
-    end
-
-    prices.each do |pr|
-      puts pr.text.slice(0..-3).to_f
-    end
-
-    # puts elements.length
-    binding.pry
+    
   end 
-
-end
-to -> #{link['href']}"
-    end
-  end
 
 end
