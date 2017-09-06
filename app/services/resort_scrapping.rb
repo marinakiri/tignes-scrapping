8690415a9bdf109ad557e505e7923da2f6208549
isarceaux <isarceaux>
2017-09-06 10:39:44 +0200

    data collection of price workingga '

--- app/services/resort_scrapping.rb
+++ app/services/resort_scrapping.rb
@@ -9,13 +9,23 @@ class ResortScrapping
     getDataFromPage
   end
 
+  def changeResort
+  end
+ 
+  def changeDate
+
+  end
+  
+  def changePage
+  end
+
   def getDataFromPage
     @url = "https://www.abritel.fr/results/rhone-alpes/champagny-en-vanoise/region:66612899/arrival:2017-12-16/departure:2017-12-23/minSleeps/1"
     @page = Nokogiri::HTML(open(@url))
     xpath_name = '//h3[@class="hit-headline"]'
     xpath_url = '//h3[@class="hit-headline"]/a'
-    xpath_prices = '//div[@class="rate"]'
-    xpath_rates = "//div[@class='price-overlay']"
+    # xpath_prices = '//div[@class="rate"]'
+    xpath_prices = "//div[@class='rate']/a"
 
     names = page.xpath(xpath_name)
     urls = page.xpath(xpath_url)
@@ -32,42 +42,11 @@ class ResortScrapping
     end
 
     prices.each do |pr|
-      puts pr.text
+      puts pr.text.slice(0..-3).to_f
     end
 
     # puts elements.length
     binding.pry
-
-
-
-
-  end
-
-  def test
-    @url = "http://ruby.bastardsbook.com/files/hello-webpage.html"
-
-    #Selection with xpath
-    puts "Selecting with xpath"
-    #'//*[@id="references"]/p[3]/a'
-
-    @page = Nokogiri::HTML(open(@url))
-    xpath1 = '//*[@id="references"]/p[3]/a'
-
-    elements = page.xpath(xpath1)
-
-    elements.each do |element|
-      puts element.text
-      puts element['href']
-    end
-
-    #Selection with xpath
-    puts ""
-    puts "Now selecting with css"
-    links = page.css('div#references a')
-
-    links.each do |link|
-      puts "For #{link.text} go to -> #{link['href']}"
-    end
-  end
+  end 
 
 end
