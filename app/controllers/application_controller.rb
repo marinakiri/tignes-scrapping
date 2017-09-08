e633cb0e16a00a8c0e36594c3a41087ccae77a70
Marina Kir <marina@MacBook-de-Marina.local>
2017-09-08 14:59:22 +0200

    def locale params in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  #before_action :authenticate_user!
+	protect_from_forgery with: :exception
+	before_action :set_locale
+	#before_action :authenticate_user!
+
+	private
+
+	def set_locale
+	I18n.locale = params[:locale] || I18n.default_locale
+	end
 end
