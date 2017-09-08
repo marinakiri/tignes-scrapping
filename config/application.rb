948216c84bad693de74d9c98f921ae887b19b6bb
Marina Kir <marina@MacBook-de-Marina.local>
2017-09-08 12:07:40 +0200

    working fr flash messages yayyyy

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module TignesScrapping
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = :sv
+    config.i18n.default_locale = :fr
   end
 end
