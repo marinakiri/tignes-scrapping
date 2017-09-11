1b9dad465111ed4929658066b6453798e57a260d
Marina Kir <marina@MacBook-de-Marina.local>
2017-09-11 12:00:49 +0200

    add pages and controller Listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  get 'stations/show'
+
+  get 'listings/show'
 
   devise_for :users
   get 'classifieds/index'
 
   root 'pages#home'
+
+  get 'stations/show'
+
+  get '/patients/:id', to: 'patients#show'
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/legal', to: 'pages#legal'
+  get '/pricing', to: 'pages#pricing'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
