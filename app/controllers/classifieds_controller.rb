9c13f07326654e21724939c076d225171c1d83ae
hong <luu.bichhong@gmail.com>
2017-09-12 12:28:38 +0200

    options for number_of_guests

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -9,16 +9,8 @@ class ClassifiedsController < ApplicationController
     @ville = params[:ville]
     @number_of_guests = params[:number_of_guests]    
     @results= Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i")
-    build_grid()
+    # SearchPrice.new(@ville, @number_of_guests).perform unless (@ville == nil || @number_of_guests == nil)
+    @average_price = Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").average("price")
   end
 
-
-  def build_grid
-    grid = PivotTable::Grid.new do |g|
-      g.source_data = @results
-      g.column_name = :title
-      g.row_name    = :start_date
-      g.value_name  = :price
-    end
-  end
 end
