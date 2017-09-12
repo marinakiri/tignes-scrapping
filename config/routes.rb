38a0850cf9eda1d263dee12f76a0e43a906e2436
isarceaux <isarceaux>
2017-09-12 22:31:49 +0200

    route added

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
   get '/dashboard', to:'users#dashboard'
 
+  mount Sidekiq::Web => ‘/sidekiq’
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
