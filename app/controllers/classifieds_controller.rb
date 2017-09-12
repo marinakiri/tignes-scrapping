d8510b67cfcd09f1e97c789b306d9fdcbf2303b2
isarceaux <isarceaux>
2017-09-12 16:50:43 +0200

    table of average setted up, tb continued

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -10,7 +10,9 @@ class ClassifiedsController < ApplicationController
     @number_of_guests = params[:number_of_guests]    
     @results= Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").order("start_date")
     # SearchPrice.new(@ville, @number_of_guests).perform unless (@ville == nil || @number_of_guests == nil)
-    @average_price = Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").average("price")
+    # @average_price = Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").average("price")
+    @averages = Classified.where("ville":params[:ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).average("price")
+    @counts = Classified.where("ville":params[:ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).count
   end
 
 end
