99f0bf95c84f5c707e1f0eff1f128d4c7e4a6b92
isarceaux <isarceaux>
2017-09-12 15:51:16 +0200

    new form change of results aspects to match specs

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -8,7 +8,7 @@ class ClassifiedsController < ApplicationController
   def search
     @ville = params[:ville]
     @number_of_guests = params[:number_of_guests]    
-    @results= Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i")
+    @results= Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").order("start_date")
     # SearchPrice.new(@ville, @number_of_guests).perform unless (@ville == nil || @number_of_guests == nil)
     @average_price = Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").average("price")
   end
