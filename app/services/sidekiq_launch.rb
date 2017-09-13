ded910cb97abc8ef5c8c9f4879349d660fcaac01
isarceaux <isarceaux>
2017-09-13 14:33:37 +0200

    shuffle put in place for regions

--- app/services/sidekiq_launch.rb
+++ app/services/sidekiq_launch.rb
@@ -2,6 +2,7 @@ class SidekiqLaunch
 
   def initialize
     regions = Resort.all.pluck(:region_number)
+    regions = regions.shuffle
 
     regions.each do |region|
       ScrappingWorker.perform_async(region)
