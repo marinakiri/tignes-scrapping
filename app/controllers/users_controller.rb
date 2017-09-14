1d511196aa3562886d094ea3e667b5ed095ecdc3
isarceaux <isarceaux>
2017-09-14 17:58:31 +0200

    blocked subscription when already subscribed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,27 @@
 class UsersController < ApplicationController
 	# before_action :authenticate_user!
+  attr_accessor :current_ville_id
 
   def dashboard
     @resorts = Resort.all
+
+    #Inscription
     s = Subscription.new
     s.user = current_user
     s.resort = Resort.find_by_ville(params["ville"])
-    s.save
+    if s.resort
+      @current_ville_id = s.resort.id
+    end
+
+    if Subscription.where(user_id:current_user.id).where(resort_id:@current_ville_id) == []
+      s.save
+    else
+      flash.now[:alert] = "Vous êtes déjà abonné(e) à cette station!"  
+    end
+
     @subscriptions = Subscription.all
     @averages = Classified.group("ville").group("start_date").group("number_of_guests").average("price")
+    @number_of_guests = params[:number_of_guests]
   end
   
 end
