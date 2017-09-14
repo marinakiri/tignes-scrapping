0abae6aca5b873763e8523c7b9d4f2e3b31a3d1c
isarceaux <isarceaux>
2017-09-14 10:57:55 +0200

    focusing the scope on trois vallees

--- app/services/sidekiq_launch.rb
+++ app/services/sidekiq_launch.rb
@@ -1,7 +1,8 @@
 class SidekiqLaunch
 
   def initialize
-    regions = Resort.all.pluck(:region_number)
+    # regions = Resort.all.pluck(:region_number)
+    regions = ['66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962']
 
     regions.each do |region|
       ScrappingWorker.perform_async(region)
