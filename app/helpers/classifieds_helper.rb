a161438cfd6180535ee5039354ddd4e5088d17a6
isarceaux <isarceaux>
2017-09-14 11:36:14 +0200

    slight changes in scrapping resorts

--- app/helpers/classifieds_helper.rb
+++ app/helpers/classifieds_helper.rb
@@ -4,11 +4,22 @@ module ClassifiedsHelper
     villes = Classified.group("ville").order("ville").pluck("ville")
     #iterate over the array
     list_of_station = []
+
     villes.each do |i|
         if Classified.where("ville":"#{i}").count > 10
           list_of_station.push(i)
         end
       end
+
+  regions = ['66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées
+  regions.each do |r|
+    puts r 
+    list_of_station << Resort.find_by_region_number(r).ville
+    puts Resort.find_by_region_number(r).ville
+  end
+
+
     return(list_of_station)
+
   end
 end
--- app/services/listing_resorts.rb
+++ app/services/listing_resorts.rb
@@ -11,29 +11,37 @@ class ListingResorts
   def get_regions_of_resorts
     
     region_links=[]
-    urls = ["https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-alpes",
-      "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-pyrenees",
-      "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-massif-central",
-      "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-vosges-jura"]
+    # urls = ["https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-alpes",
+    #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-pyrenees",
+    #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-massif-central",
+    #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-vosges-jura"]
+      urls = ['https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski']
     urls.each do |url|
       resp = HTTParty.get(url); nil
       region_links = region_links + resp.split("\n").grep(/<li><a href="\/annonces\/location-vacances\/.*php/).map{|href| href.split("\"")[1]}
     end
     
     region_links.each do |region_link|
-      link = "https://www.abritel.fr" + region_link
-      get_regions_from_links(link)
+      get_regions_from_links(region_link)
     end
 
   end
 
-  def get_regions_from_links(link)
-    resp = HTTParty.get(link); nil
-    if resp.split("\n").grep(/<link rel=\"next\"/)[0] != nil
-      current_resort = Resort.new
-      current_resort.url = link
-      current_resort.region_number = resp.split("\n").grep(/<link rel=\"next\"/)[0].split("/")[4].split(":")[1]
-      current_resort.save
+  def get_regions_from_links(region_link)
+    link = "https://www.abritel.fr" + region_link
+    puts region_link
+    if link =~ URI::regexp
+      resp = HTTParty.get(link); nil
+      if resp.split("\n").grep(/<link rel=\"next\"/)[0] != nil && Resort.find_by_region_number(region_link) == nil
+        current_resort = Resort.new
+        current_resort.url = link
+        current_resort.region_number = resp.split("\n").grep(/<link rel=\"next\"/)[0].split("/")[4].split(":")[1]
+        current_resort.save
+      elsif Resort.find_by_region_number(region_link) != nil
+        puts "already in database"
+      end
+    else
+      puts "not valid url"
     end
   end
 
