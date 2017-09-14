51bafe690250c83f327de691f484e2dce535466f
hong <luu.bichhong@gmail.com>
2017-09-14 18:16:29 +0200

    add h1,2, change search view

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -1,5 +1,5 @@
 class ClassifiedsController < ApplicationController
-  attr_accessor :average_price
+  attr_accessor :current_ville
 
   def index
     @classifieds = Classified.all
@@ -8,12 +8,13 @@ class ClassifiedsController < ApplicationController
   def search
     @classifieds = Classified.all
     @ville = params[:ville]
+    @current_ville = @ville
     @number_of_guests = params[:number_of_guests]    
-    @results= Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").order("start_date")
+    @results= Classified.where("ville":"#{@current_ville}").where("number_of_guests":"#{@number_of_guests}.to_i").order("start_date")
     # SearchPrice.new(@ville, @number_of_guests).perform unless (@ville == nil || @number_of_guests == nil)
     # @average_price = Classified.where("ville":"#{@ville}").where("number_of_guests":"#{@number_of_guests}.to_i").average("price")
-    @averages = Classified.where("ville":params[:ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).average("price")
-    @counts = Classified.where("ville":params[:ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).count
+    @averages = Classified.where("ville":params[:current_ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).average("price")
+    @counts = Classified.where("ville":params[:current_ville]).group("start_date").where("number_of_guests":params[:number_of_guests]).count
   end
 
 end
