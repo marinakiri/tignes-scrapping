e0718c63615b3948ccbf79a973338be3cb529893
isarceaux <isarceaux>
2017-09-14 12:10:21 +0200

    better way to scrapp the list of resorts, and focus on 3 vallees

--- app/helpers/classifieds_helper.rb
+++ app/helpers/classifieds_helper.rb
@@ -5,11 +5,11 @@ module ClassifiedsHelper
     #iterate over the array
     list_of_station = []
 
-    villes.each do |i|
-        if Classified.where("ville":"#{i}").count > 10
-          list_of_station.push(i)
-        end
-      end
+    # villes.each do |i|
+    #     if Classified.where("ville":"#{i}").count > 10
+    #       list_of_station.push(i)
+    #     end
+    #   end
 
   regions = ['66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées
   regions.each do |r|
--- app/services/listing_resorts.rb
+++ app/services/listing_resorts.rb
@@ -4,6 +4,7 @@ class ListingResorts
 
   def initialize
     p "Let's get the region code from all French ski Resorts!"
+    # Resort.delete_all
     @region_hash = {}
     get_regions_of_resorts
   end
@@ -15,7 +16,7 @@ class ListingResorts
     #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-pyrenees",
     #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-massif-central",
     #   "https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski/ski-vosges-jura"]
-      urls = ['https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski']
+    urls = ['https://www.abritel.fr/info/guide/idees/vacances-montagne/vacances-ski']
     urls.each do |url|
       resp = HTTParty.get(url); nil
       region_links = region_links + resp.split("\n").grep(/<li><a href="\/annonces\/location-vacances\/.*php/).map{|href| href.split("\"")[1]}
@@ -29,15 +30,20 @@ class ListingResorts
 
   def get_regions_from_links(region_link)
     link = "https://www.abritel.fr" + region_link
-    puts region_link
-    if link =~ URI::regexp
+    puts link
+    if link =~ /\A#{URI::regexp}\z/
+      puts "valid url"
       resp = HTTParty.get(link); nil
-      if resp.split("\n").grep(/<link rel=\"next\"/)[0] != nil && Resort.find_by_region_number(region_link) == nil
+      if resp.split("\n").grep(/<link rel=\"next\"/)[0] != nil && Resort.find_by_region_number(resp.split("\n").grep(/<link rel=\"next\"/)[0].split("/")[4].split(":")[1]) == nil
         current_resort = Resort.new
         current_resort.url = link
         current_resort.region_number = resp.split("\n").grep(/<link rel=\"next\"/)[0].split("/")[4].split(":")[1]
+        #Adding city
+        indice_url = link.split("_").length - 2
+        current_resort.ville_url = link.split("_")[indice_url]
+        current_resort.ville = URI.unescape(link.split("_")[indice_url])
         current_resort.save
-      elsif Resort.find_by_region_number(region_link) != nil
+      elsif Resort.find_by_region_number(resp.split("\n").grep(/<link rel=\"next\"/)[0].split("/")[4].split(":")[1]) != nil
         puts "already in database"
       end
     else
