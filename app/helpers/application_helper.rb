633dee6605e994ab18cae37489845150cd02032d
isarceaux <isarceaux>
2017-09-17 23:49:38 +0200

    2 cities also added the list station helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     villes = Classified.group("ville").order("ville").pluck("ville")
     #iterate over the array
     list_of_station = []
-    regions = ['66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées
+    regions = ['66612889','66612807','66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées + Tignes et Val d'Isère
 
     villes.each do |i|
       if Resort.find_by_ville(I18n.transliterate(i))
