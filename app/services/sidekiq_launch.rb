90fda0635e458e17f35d2d57377eecc3c65c5255
isarceaux <isarceaux>
2017-09-17 23:46:41 +0200

    addition of tignes and val d'isere to the list to be scrapped again

--- app/services/sidekiq_launch.rb
+++ app/services/sidekiq_launch.rb
@@ -2,8 +2,7 @@ class SidekiqLaunch
 
   def initialize
     # regions = Resort.all.pluck(:region_number)
-    regions = ['66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées
-    regions = regions.shuffle
+    regions = ['66612889','66612807','66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées + Tignes et Val d'Isère
     regions.each do |region|
       # ScrappingWorker.perform_async(region)
       ScrappingWorker.perform_async(region)
