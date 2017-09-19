02e4006f0e36326012482884a370e4e820c18833
Marina Kir <marina@MacBook-de-Marina.local>
2017-09-19 16:47:51 +0200

    remove anchor from scroll-top

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,9 @@
-var $root = $('html, body');
-$('a').click(function() {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+$('#scrollTop').on('click', function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");  
+  return false;
 });
+
+// $("scroll-top").click(function() {  
+//   $("html, body").animate({ scrollTop: 0 }, "slow");  
+//   return false;  
+// });
