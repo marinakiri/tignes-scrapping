5121f7be40d601ea0c4882d8a4a443674cb5e549
isarceaux <isarceaux>
2017-09-22 10:30:33 +0200

    cleaning of the project, removed unecessary files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,19 +7,17 @@ module ApplicationHelper
     regions = ['66612889','66612807','66612786','66612960','66612917','66612918','66612972','66612919','66612803','66612962'] #Les trois vallées + Tignes et Val d'Isère
 
     villes.each do |i|
-      if Resort.find_by_ville(I18n.transliterate(i))
-         if Classified.where("ville":"#{i}").count > 10 && (regions.include? Resort.find_by_ville(I18n.transliterate(i)).region_number)
-           list_of_station.push(titleize(i))
+      if i
+        if Resort.find_by_ville(I18n.transliterate(i))
+           if Classified.where("ville":"#{i}").count > 10 && (regions.include? Resort.find_by_ville(I18n.transliterate(i)).region_number)
+             list_of_station.push(titleize(i))
+          end
         end
       end
     end
 
     return(list_of_station)
 
-  end
-
-  def list_of_number_of_guests
-      return Classified.where(ville:@search.ville).group("number_of_guests").count.keys
   end
 
   def titleize(title)
