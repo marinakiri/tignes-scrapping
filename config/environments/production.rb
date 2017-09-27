9697b1b9dce6c39a39bc95164b713c72c92c2f67
isarceaux <isarceaux>
2017-09-27 21:58:54 +0200

    change of default host to algo.ski instead of heroku... let's see'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Configuring mailer to work in production
-  config.action_mailer.default_url_options = { :host => 'algoski.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'algo.ski' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
