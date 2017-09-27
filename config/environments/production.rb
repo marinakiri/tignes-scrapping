f6a7c775dc0e4d48a11cde9a9b6062efcdaf1b71
isarceaux <isarceaux>
2017-09-27 17:43:19 +0200

    production.rb config modified to stick to the tuto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,22 +89,24 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #Configuring mailer to work in production
   config.action_mailer.default_url_options = { :host => 'algoski.herokuapp.com' }
 
   Rails.application.routes.default_url_options[:host] = 'algoski.herokuapp.com'
 
-  elivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = 
-  {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
 
 end
